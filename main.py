4afb06a6711eddaf6486f32af4cf20c1bee9e939
Dmitry Mordanov <mordanov314@gmail.com>
2023-09-01 15:37:48 +0300

    final commit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
-from flask import Flask, render_template, send_from_directory
+from flask import Flask, render_template, send_from_directory, request, redirect, make_response
+
 
 app = Flask(__name__)
 
@@ -8,9 +9,23 @@ app = Flask(__name__)
 def base():
     return render_template('index.html')
 
-@app.route('/about/')
-def about():
-    return render_template('about.html')
+
+@app.post('/submit')
+def submit():
+    name = request.form.get('name')
+    context = {'name': name}
+    response = make_response(render_template('request.html', **context))
+    response.headers['new_cookie'] = 'request submitted'
+    response.set_cookie('username', name)
+    return response
+
+
+@app.post('/exit')
+def my_exit():
+    response = make_response(redirect('/'))
+    response.delete_cookie('username')
+    return response
+
 
 @app.route('/favicon.ico')
 def favicon():
@@ -21,4 +36,3 @@ def favicon():
 
 if __name__ == '__main__':
     app.run(debug=True)
-
